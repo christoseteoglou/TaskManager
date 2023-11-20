d59615d68b4e39cb6d8e827f57db61b6e8a0a751
christoseteoglou <itsmeglue@gmail.com>
2023-11-20 13:31:31 +0100

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Christos Eteoglou on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
